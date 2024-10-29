47e6993a5c06fb5c72af5f5cd464a0747966392f
AlaeddineJendoubi <alaeddine.jendoubi@elco-solutions.de>
2024-10-29 03:10:43 +0100

    setup auth service (init session/refresh / logout)

---
+++ app/services/auth/index.ts
@@ -0,0 +1,53 @@
+import {Api, removeAuthorizationTokens} from '../../api/client';
+import {SignInDataType} from '../../api/types';
+import StorageService from '../storage';
+
+class AuthService {
+  initSession = async (refreshToken: string, accessToken: string) => {
+    try {
+      if (accessToken) {
+        await StorageService.setDataToStorage('accessToken', accessToken);
+      } else {
+        console.log('No access token provided.');
+      }
+
+      if (refreshToken) {
+        await StorageService.setDataToStorage('refreshToken', refreshToken);
+      } else {
+        console.log('No refresh token provided.');
+      }
+    } catch (e) {
+      console.error('Something went wrong while storing tokens:', e);
+    }
+  };
+  signInUserService = async (signInData: SignInDataType) => {
+    try {
+      const responseData = await Api?.auth.signInUserRequest(signInData);
+      const {refreshToken, accessToken} = responseData?.data;
+      this.initSession(refreshToken, accessToken);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  refreshSession = async (refreshTokenStored: string) => {
+    try {
+      if (refreshTokenStored) {
+        const responseData = await Api?.auth?.refreshUserToken({
+          refreshToken: refreshTokenStored,
+        });
+
+        const {refreshToken, accessToken} = responseData?.data;
+        this.initSession(refreshToken, accessToken);
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+  signOutUserService = async () => {
+    await StorageService.resetStorage();
+    removeAuthorizationTokens();
+  };
+}
+
+export default new AuthService();
