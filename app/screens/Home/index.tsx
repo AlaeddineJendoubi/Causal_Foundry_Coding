818f608bfcf99d337b37cf1d0ae3687480887f62
AlaeddineJendoubi <alaeddine.jendoubi@elco-solutions.de>
2024-10-29 03:31:47 +0100

    Create Logout button

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -1,13 +1,32 @@
-import {Text} from '@ui-kitten/components';
 import React, {FC} from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {ButtonIcon} from '../../components/ButtonIcon';
+import {useLogout} from '../../hooks/useLogout';
 
 export const Home: FC = () => {
+  const {isLoading, logout} = useLogout();
   return (
-    <View>
-      <Text style={{alignSelf: 'center'}} category="h2">
-        This is Home
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <ButtonIcon
+          isLoading={isLoading}
+          text="Logout"
+          status="danger"
+          iconName="log-out-outline"
+          iconColor="white"
+          onPress={() => logout()}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  buttonContainer: {
+    padding: 16,
+  },
+});
