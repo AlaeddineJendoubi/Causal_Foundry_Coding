226b56d5d59494228d78d89068aec91884b46d75
AlaeddineJendoubi <alaeddine.jendoubi@elco-solutions.de>
2024-10-29 03:10:56 +0100

    update login handler hook

---
+++ app/screens/Login/hooks/useHandleForm.ts
@@ -0,0 +1,47 @@
+import {useState} from 'react';
+import {useAuthUser} from '../../../hooks/useAuthUser';
+import {useNavigation} from '@react-navigation/native';
+
+export const useHandleForm = () => {
+  const [userName, setUserName] = useState<string>('');
+  const [userPassword, setUserPassword] = useState<string>('');
+  const {reset} = useNavigation();
+  const {refetch, error, isFetching} = useAuthUser({
+    username: userName,
+    password: userPassword,
+  });
+  const isFormValid = userName?.length > 0 && userPassword?.length > 0;
+
+  const handleUserName = (inputUserName: string) => {
+    setUserName(inputUserName);
+  };
+
+  const handleUserPassword = (inputUserPassword: string) => {
+    setUserPassword(inputUserPassword);
+  };
+
+  const handleLogin = async () => {
+    if (isFormValid) {
+      try {
+        await refetch();
+        reset({
+          index: 0,
+          routes: [{name: 'Home' as never}], // Navigate to Home screen and reset the stack
+        });
+      } catch (e) {
+        console.log('err', e);
+      }
+    }
+  };
+
+  return {
+    isLoading: isFetching,
+    error,
+    handleLogin,
+    isFormValid,
+    userName,
+    userPassword,
+    handleUserName,
+    handleUserPassword,
+  };
+};
