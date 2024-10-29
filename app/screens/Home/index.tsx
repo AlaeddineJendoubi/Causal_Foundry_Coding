8937ac6f9a72f8b65b10917f94f1c686c59216e9
AlaeddineJendoubi <alaeddine.jendoubi@elco-solutions.de>
2024-10-29 03:59:47 +0100

    update home screen (add user name)

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -1,12 +1,27 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {ButtonIcon} from '../../components/ButtonIcon';
 import {useLogout} from '../../hooks/useLogout';
+import {useFetchUserData} from '../../hooks/useFetchUserData';
+import {useFocusEffect} from '@react-navigation/native';
+import {Text} from '@ui-kitten/components';
+import {Spacer} from '../../components/Spacer';
 
 export const Home: FC = () => {
   const {isLoading, logout} = useLogout();
+
+  const {getUserData, userData} = useFetchUserData();
+
+  useEffect(() => {
+    !userData && getUserData();
+  }, []);
   return (
     <View style={styles.container}>
+      <View>
+        <Spacer size={10} />
+        <Text style={styles?.textStyle}>Welcome {userData?.firstName}</Text>
+        <Spacer line size={10} />
+      </View>
       <View style={styles.buttonContainer}>
         <ButtonIcon
           isLoading={isLoading}
@@ -25,6 +40,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'space-between',
+  },
+  textStyle: {
+    textAlign: 'center',
   },
   buttonContainer: {
     padding: 16,
