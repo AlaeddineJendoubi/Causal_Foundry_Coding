fac6564eccab4ddf2d80c6f2dc3005c5070c4009
AlaeddineJendoubi <alaeddine.jendoubi@elco-solutions.de>
2024-10-29 03:09:17 +0100

    Create login Form

--- app/screens/Login/index.tsx
+++ app/screens/Login/index.tsx
@@ -1,13 +1,83 @@
-import {Text} from '@ui-kitten/components';
-import React, {FC} from 'react';
-import {View} from 'react-native';
+import {Icon, Text} from '@ui-kitten/components';
+import React, {FC, useEffect} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {InputField} from '../../components/InputField';
+import {Spacer} from '../../components/Spacer';
+import {useHandleForm} from './hooks/useHandleForm';
+import {ButtonIcon} from '../../components/ButtonIcon';
 
 export const Login: FC = () => {
+  const {
+    isFormValid,
+    handleUserName,
+    handleUserPassword,
+    handleLogin,
+    isLoading,
+  } = useHandleForm();
+
+  const dummyData = {username: 'michaelw', password: 'michaelwpass'};
+
+  useEffect(() => {
+    handleUserName(dummyData?.username);
+    handleUserPassword(dummyData?.password);
+  });
   return (
-    <View>
-      <Text style={{alignSelf: 'center'}} category="h2">
-        This is Login
+    <View style={styles?.mainContainerStyle}>
+      <Icon style={styles?.icon} fill="#8F9BB3" name="person-outline" />
+      <Spacer size={5} />
+
+      <Text category="h4" style={styles?.titleStyle}>
+        Sign in
       </Text>
+      <Spacer size={10} />
+
+      <View style={styles?.formContainer}>
+        <InputField
+          value={dummyData?.username}
+          onChangeText={handleUserName}
+          hasIcon
+          iconName="person-outline"
+          placeholder="User Name"
+        />
+        <Spacer size={10} />
+        <InputField
+          value={dummyData?.password}
+          onChangeText={handleUserPassword}
+          hasIcon
+          isSecure
+          placeholder="Password"
+        />
+        <Spacer line size={20} />
+
+        <ButtonIcon
+          iconColor="white"
+          text="Sign in "
+          iconName="log-in-outline"
+          style={styles?.buttonStyle}
+          disabled={!isFormValid}
+          isLoading={isLoading}
+          onPress={() => handleLogin()}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mainContainerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleStyle: {},
+  formContainer: {
+    width: '90%',
+  },
+  icon: {
+    width: 50,
+    height: 50,
+  },
+  buttonStyle: {
+    marginHorizontal: '30%',
+  },
+});
