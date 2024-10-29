d670ae027a98ec386b72db5f7ea7117f56d9d24c
AlaeddineJendoubi <alaeddine.jendoubi@elco-solutions.de>
2024-10-29 07:40:14 +0100

    Create search input component

---
+++ app/components/SearchInput/index.tsx
@@ -0,0 +1,35 @@
+import React, {FC, useState} from 'react';
+
+import {Input, InputProps} from '@ui-kitten/components';
+import {ActionIcon} from '../ActionIcon';
+import {StyleSheet} from 'react-native';
+
+interface SearchInputProps extends InputProps {
+  clearText: () => void;
+  searchText: string;
+  isEmptyField: boolean;
+}
+export const SearchInput: FC<SearchInputProps> = ({
+  clearText,
+  searchText,
+  isEmptyField,
+  ...rest
+}) => {
+  console.log(searchText?.length);
+
+  const searchAccessory = () => (
+    <ActionIcon
+      name={isEmptyField ? 'search-outline' : 'close-outline'}
+      action={clearText}
+      style={styles?.icon}
+    />
+  );
+
+  return <Input {...rest} accessoryRight={searchAccessory} />;
+};
+const styles = StyleSheet.create({
+  icon: {
+    width: 25,
+    height: 25,
+  },
+});
