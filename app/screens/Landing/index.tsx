e9f92b07ce48fa33e241ff86f8b9ec0695894bb2
AlaeddineJendoubi <alaeddine.jendoubi@elco-solutions.de>
2024-10-29 03:13:53 +0100

    update landing screen (use useSession hook)

--- app/screens/Landing/index.tsx
+++ app/screens/Landing/index.tsx
@@ -1,7 +1,10 @@
 import React, {FC} from 'react';
 import {ActivityIndicator, View} from 'react-native';
+import {useSession} from '../../hooks/useSession';
 
 export const LandingScreen: FC = () => {
+  useSession();
+
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <ActivityIndicator size={'large'} />
