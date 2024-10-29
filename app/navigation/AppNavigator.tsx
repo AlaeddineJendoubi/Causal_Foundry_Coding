17e07d437784a4c9752d8950ac66f4bd63ad2042
AlaeddineJendoubi <alaeddine.jendoubi@elco-solutions.de>
2024-10-29 03:05:53 +0100

    setup navigation stack

---
+++ app/navigation/AppNavigator.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Login} from '../screens/Login';
+import {Home} from '../screens/Home';
+import {MainContainer} from '../containers/mainContainer';
+import {LandingScreen} from '../screens/Landing';
+
+const Stack = createNativeStackNavigator();
+
+export const AppNavigator = () => {
+  return (
+    <MainContainer>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Landing">
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="Login"
+            component={Login}
+          />
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="Home"
+            component={Home}
+          />
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="Landing"
+            component={LandingScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </MainContainer>
+  );
+};
